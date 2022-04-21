4db4865f034ce7bc2f38a52f976b1c081000293a
Vova <vovakurlichenko@gmail.com>
2022-04-21 12:07:20 +0300

    click logo

--- src/components/AppHeader/AppHeader.tsx
+++ src/components/AppHeader/AppHeader.tsx
@@ -2,28 +2,34 @@ import React from 'react';
 import style from './AppHeader.module.css';
 import { Logo } from '@ya.praktikum/react-developer-burger-ui-components';
 import Button from './Button/Button';
+import { NavLink } from 'react-router-dom';
 
 const AppHeader = () => {
-    return(
+    return (
         <header className={style.header}>
             <div className={style.header_content}>
                 <nav className={style.header_nav}>
                     <Button
                         icon={"BurgerIcon"}
-                        text={"Конструстор"}/>
+                        text={"Конструстор"} />
                     <Button
                         data-cy='feed-page-link'
                         icon={"ListIcon"}
-                        text={"Лента заказов"}/>
+                        text={"Лента заказов"} />
                 </nav>
-                <Logo/>
+                <NavLink
+                    to="/"
+                    exact={true}
+                >
+                    <Logo />
+                </NavLink>
                 <div className={style.header_button_profile}>
                     <Button
                         icon={"ProfileIcon"}
-                        text={"Личный кабинет"}/>
+                        text={"Личный кабинет"} />
                 </div>
             </div>
-        </header> 
+        </header>
     );
 }
 
